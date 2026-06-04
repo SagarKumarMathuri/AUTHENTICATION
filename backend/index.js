d926c3cd3a7bebd509b361c85874d7c2263efe94
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-04 13:02:20 +0530

    db file and some changes inside insde

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+await connectDB();
+const app = express();
+
+const PORT = process.env.PORT 
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
