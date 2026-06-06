ee21bdfdfdc4084f6e8f9623002db6612059125f
sagar kumar mathuri <sagarku2002@gmail.com>
2026-06-06 11:51:38 +0530

    loginUser

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,12 +1,13 @@
 import { registerSchema } from "../config/zod.js";
-import {redisClient} from "../index.js";
+import { redisClient } from "../index.js";
 import TryCatch from "../middlewares/TryCatch.js";
 import sanitize from "mongo-sanitize";
-import {User} from "../models/User.js"
-import bcrypt from 'bcrypt'
-import crypto from 'crypto'
-import sendMail from '../config/sendMail.js'
-import {getVerifyEmailHtml} from "../config/html.js"
+import { User } from "../models/User.js";
+import bcrypt from "bcrypt";
+import crypto from "crypto";
+import sendMail from "../config/sendMail.js";
+import { getVerifyEmailHtml } from "../config/html.js";
+import { email } from "zod";
 
 export const registerUser = TryCatch(async (req, res) => {
   const sanitezedBody = sanitize(req.body);
@@ -17,94 +18,136 @@ export const registerUser = TryCatch(async (req, res) => {
     const zodError = validation.error;
 
     let firstErrorMessage = "Validation failed";
-    let allErrors = []
-    if(zodError?.issues && Array.isArray(zodError.issues)){
+    let allErrors = [];
+    if (zodError?.issues && Array.isArray(zodError.issues)) {
       allErrors = zodError.issues.map((issue) => ({
-        field : issue.path ? issue.path.join(".") : "unknown",
+        field: issue.path ? issue.path.join(".") : "unknown",
         message: issue.message || "Validation Error",
         code: issue.code,
-      }))
+      }));
       firstErrorMessage = allErrors[0]?.message || "Validation Error";
     }
     return res.status(400).json({
       message: firstErrorMessage,
-      error: allErrors
+      error: allErrors,
     });
   }
 
   const { name, email, password } = validation.data;
 
   const ratelimitKey = `register-rate-limit:${req.ip}:${email}`;
-  
+
   if (await redisClient.get(ratelimitKey)) {
     return res.status(429).json({
-      message:"Too many requests, try again later",
-    })
+      message: "Too many requests, try again later",
+    });
   }
 
-  const existingUser = await User.findOne({ email});
+  const existingUser = await User.findOne({ email });
 
   if (existingUser) {
     return res.status(400).json({
-      message:"User already exists",
-    })
+      message: "User already exists",
+    });
   }
- 
+
   const hashPassword = await bcrypt.hash(password, 10);
 
   const verifyToken = crypto.randomBytes(32).toString("hex");
 
-  const verifyKey =   `verify:${verifyToken}`
-  
+  const verifyKey = `verify:${verifyToken}`;
+
   const datatoStore = JSON.stringify({
     name,
     email,
-    password:hashPassword,
-  })
+    password: hashPassword,
+  });
 
-  await redisClient.set(verifyKey, datatoStore, {EX: 300});
+  await redisClient.set(verifyKey, datatoStore, { EX: 300 });
 
-  const subject = "verify your email for Account creation"
-  
-  const html =  getVerifyEmailHtml({email , token: verifyToken});
+  const subject = "verify your email for Account creation";
 
-  await sendMail({email, subject, html});
+  const html = getVerifyEmailHtml({ email, token: verifyToken });
 
-  await redisClient.set(ratelimitKey, "true", {EX: 60})
+  await sendMail({ email, subject, html });
+
+  await redisClient.set(ratelimitKey, "true", { EX: 60 });
 
   res.json({
-    message:"If your email is valid, a verification like has been sent. it will expire in 5 minutes",
+    message:
+      "If your email is valid, a verification like has been sent. it will expire in 5 minutes",
   });
 });
 
-
-export const verifyUser = TryCatch(async(req, res) =>{
-  const {token} = req.params;
-  if(token){
+export const verifyUser = TryCatch(async (req, res) => {
+  const { token } = req.params;
+  if (!token) {
     return res.status(400).json({
-      message:"Verification token is required.",
-    })
+      message: "Verification token is required.",
+    });
   }
 
   const verifyKey = `verify:${token}`;
 
-  const userDataJson = await redisClient.get(verifyKey)
+  const userDataJson = await redisClient.get(verifyKey);
 
-  if(!userDataJson) {
+  if (!userDataJson) {
     return res.status(400).json({
-      message:"Verification Link is expired",
-    })
+      message: "Verification Link is expired",
+    });
   }
- 
+
   await redisClient.del(verifyKey);
 
-  const userData = JSON.parse(userDataJson)
-  
-  const existingUser = await User.findOne({ email: userData.email});
+  const userData = JSON.parse(userDataJson);
+
+  const existingUser = await User.findOne({ email: userData.email });
 
   if (existingUser) {
     return res.status(400).json({
-      message:"User already exists",
-    })
+      message: "User already exists",
+    });
   }
-})
+
+  const newUser = await User.create({
+    name: userData.name,
+    email: userData.email,
+    password: userData.password,
+  });
+
+  res.status(201).json({
+    message: "Email verified sucessfully! your account has been created",
+    user: {
+      _id: newUser._id,
+      name: newUser.name,
+      email: newUser.email,
+    },
+  });
+});
+
+export const loginUser = TryCatch(async (req, res) => {
+  const sanitezedBody = sanitize(req.body);
+
+  const validation = registerSchema.safeParse(sanitezedBody);
+
+  if (!validation.success) {
+    const zodError = validation.error;
+
+    let firstErrorMessage = "Validation failed";
+    let allErrors = [];
+    if (zodError?.issues && Array.isArray(zodError.issues)) {
+      allErrors = zodError.issues.map((issue) => ({
+        field: issue.path ? issue.path.join(".") : "unknown",
+        message: issue.message || "Validation Error",
+        code: issue.code,
+      }));
+      firstErrorMessage = allErrors[0]?.message || "Validation Error";
+    }
+    return res.status(400).json({
+      message: firstErrorMessage,
+      error: allErrors,
+    });
+  }
+
+  const {email, password } = validation.data;
+});
